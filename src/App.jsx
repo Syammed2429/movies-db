9c6c4e69b18394aaa324e08b5a7a565dd3e43c3c
Dada Khalandar <kerry.blig12@gmail.com>
2021-12-03 21:53:09 +0530

    movies db

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import SearchMovies from './components/SearchMovies/SearchMovies';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to movies Data Base</h1>
+      <SearchMovies />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/SearchMovies/SearchMovies.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const SearchMovies = () => {
+    return (
+        <div>
+            <input type="text" placeholder="Search Your Movie" />
+            <button>Search</button>
+        </div>
+    )
+}
+
+export default SearchMovies
